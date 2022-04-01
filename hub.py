2757dd93e135bc0ed1fa5241aad2f59c98e9fd4d
Derek Laventure <derek@consensus.enterprises>
2022-04-01 13:30:57 -0300

    Refactor API pagination into a python iterator class. Also fix state-handling, allowing first successful import to AzDo.

--- hub.py
+++ hub.py
@@ -4,6 +4,10 @@ Functions relevant to ZenHub/GitHub source.
 """
 
 import requests
+from common import parse_date
+
+# DEBUG
+from rich import print as rprint
 
 def query_github_issues(query, config):
     print('Retrieving issues.... ' + query)
@@ -22,32 +26,12 @@ def query_zenhub_issue(repo_name, issue_num, config):
     print(f'Retrieving ZenHub data for {repo_name}: {issue_num} ({zenhub_issue_url})')
     return requests.get(zenhub_issue_url, verify=False)
 
-def hub_row_data(issue, tags, assignees, priority, DateCreated, DateUpdated, estimate):
-    row_dict = {
-        'type': 'Epic' if ("is_epic" in issue and issue['is_epic']) else 'Product Backlog Item',
-        'title': issue['title'],
-        # @TODO: append a link back to GitHub issue to description
-        'body': str(issue['body']) + "\n\nOriginal GitHub issue link: " + str(issue['url']),
-        'tags': tags,
-        'assignee': assignees[:-1],
-        'priority': priority,
-        'state': issue['state'],
-        'author': issue['user']['login'],
-        'created': DateCreated,
-        'changed': DateUpdated,
-        'effort': estimate,
-        'iteration': 'ISSUE IMPORT TEST PROJECT'
-    }
-    return row_dict
-
 def get_repo_id(repo_name, config):
-    repo_json = get_github_api(f'repos/{repo_name}', config).json()
+    repo_json = get_github_api(github_api_base() + f'repos/{repo_name}', config).json()
     return repo_json['id']
 
-def get_github_api(endpoint, config):
-    return get_github_api_raw(github_api_base() + endpoint, config)
-
-def get_github_api_raw(url, config):
+def get_github_api(url, config):
+    """Takes a full API url and queries it with auth details."""
     return requests.get(url, auth=config['GITHUB_AUTH'], verify=False)
 
 def github_api_base():
@@ -55,3 +39,126 @@ def github_api_base():
 
 def zenhub_api_base():
     return 'https://api.zenhub.io/p1/repositories/'
+
+class HubIssues:
+    """Iterator class over ZenHub issues"""
+
+    def __init__(self, repo_name, config):
+        self.config = config
+        self.last_page = False
+        self.repo_name = repo_name
+        self.query_url = github_api_base() + f'repos/{repo_name}/issues'
+        #self.query_url = 'https://api.github.com/repositories/332794551/issues?page=4'
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.query_url == None:
+            raise StopIteration
+
+        # Start by getting the current batch of issues from the API
+        print(f"Next iteration: {self.query_url}")
+        issues = query_github_issues(self.query_url, self.config)
+
+        # Then loop over them and collect the ZenHub data for each.
+        issues_list = []
+        for issue in issues.json():
+            # Ignore pull requests, which github treats as issues
+            if 'pull_request' in issue:
+                continue
+
+            zen_r = query_zenhub_issue(self.repo_name, issue['number'], self.config).json()
+
+            print(f'{self.repo_name} issue: ' + str(issue['number']))
+            issues_list.append(self.process_issue(issue, zen_r))
+
+        # Now set up the next page of github issues (if any).
+        self.set_new_query_url(issues)
+
+        # And return the current list of issues.
+        return issues_list
+
+    def set_new_query_url(self, issues):
+        """Take a recent issues request, check for a next page of results to query."""
+
+        # If the link header is missing, we're done.
+        if not 'link' in issues.headers:
+            self.query_url = None
+            return
+
+        # Parse the link header to get a list of pages.
+        pages = self.get_pages(issues.headers['link'])
+
+        # If we're already on the last page, we're done.
+        if not "next" in pages:
+            self.query_url = None
+            return
+
+        # Otherwise, request the next url.
+        self.query_url = pages['next']
+
+
+    def process_issue(self, issue, zen_r):
+        # DEBUG
+        print("Processing issue... " + str(issue['url']))
+        #rprint(issue)
+        #rprint(zen_r)
+
+        DateCreated = parse_date(issue['created_at'])
+        DateUpdated = parse_date(issue['updated_at'])
+
+        assignees, tags = '', ''
+
+        for i in issue['assignees'] if issue['assignees'] else []:
+            assignees += i['login'] + ','
+
+        for x in issue['labels'] if issue['labels'] else []:
+            tags += x['name'] + ','
+
+        if not issue.get('body'):
+            body = ''
+        else:
+            body = str(issue['body'])
+
+        body += "\n\nOriginal GitHub issue link: " + str(issue['url'])
+        issue['body'] = body
+
+        issue['estimate'] = zen_r.get('estimate', dict()).get('value', "")
+        issue['is_epic'] = zen_r['is_epic'] if "is_epic" in zen_r else False
+
+        Pipeline = zen_r.get('pipeline', dict()).get('name', "")
+        print("PIPELINE VALUE: " + Pipeline)
+        if Pipeline == 'Closed' or Pipeline == 'Done':
+            issue['state'] = 'Done'
+        else:
+            issue['state'] = 'New'
+
+        row_dict = self.hub_row_data(issue, tags, assignees, DateCreated, DateUpdated)
+        return row_dict
+
+    def hub_row_data(self, issue, tags, assignees, DateCreated, DateUpdated):
+        row_dict = {
+            'type': 'Epic' if ("is_epic" in issue and issue['is_epic']) else 'Product Backlog Item',
+            'title': issue['title'],
+            #'body': str(issue['body']) + "\n\nOriginal GitHub issue link: " + str(issue['url']),
+            'body': issue['body'],
+            'tags': tags,
+            #'assignee': assignees[:-1],
+            'assignee': '',
+            'priority': 2,
+            'state': issue['state'],
+            #'author': issue['user']['login'],
+            'author': '',
+            'created': DateCreated,
+            'changed': DateUpdated,
+            'effort': issue['estimate'],
+            'iteration': 'ISSUE IMPORT TEST PROJECT'
+        }
+        return row_dict
+
+    def get_pages(self, link_header):
+        return dict(
+            [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
+                [link.split(';') for link in
+                link_header.split(',')]])
--- zenhub-export.py
+++ zenhub-export.py
@@ -7,7 +7,6 @@ into Azure DevOps.
 Supports Github API v3 and ZenHubs current working API.
 """
 import csv
-import datetime
 import sys
 
 # Set utf-8 encoding everywhere (https://stackoverflow.com/a/63573649)
@@ -17,19 +16,18 @@ sys.stdout.reconfigure(encoding='utf-8')
 # DEBUG
 from rich import print as rprint
 
-from common import get_config, get_column_headers, prepare_row, parse_date
-from hub import query_github_issues, query_zenhub_issue, hub_row_data, get_github_api_raw
+from common import get_config, get_column_headers, prepare_row
+from hub import HubIssues
 
 def main():
     config = get_config()
+    
+    # @TODO: Is this doing anything?
     csv.register_dialect('azdo', 'excel',  doublequote=False, escapechar='\\')
 
-    # @TODO: refactor this common stuff
-    # @TODO: move headers into config? or make them 
-
     # @TODO: use with idiom here (no need to close in that case)
-
     OPENFILE = open(config['FILENAME'], 'w', newline='\n', encoding="utf-8")
+    # @TODO: move headers into config? or make them 
     FILEOUTPUT = csv.DictWriter(OPENFILE, fieldnames=get_column_headers().values(), dialect='excel', quoting=csv.QUOTE_ALL)
     FILEOUTPUT.writeheader()
     config['FILEOUTPUT'] = FILEOUTPUT
@@ -44,96 +42,13 @@ def get_issues(repo_data, config):
     repo_ID = repo_data[1]
 
   #  issues_for_repo = query if query is not None else f'repos/{repo_name}/issues?' + config["QUERY"]
-    issues = query_github_issues(f'repos/{repo_name}/issues', config)
-
-    # @TODO: wrap this in an iterator
-
-    count = len(issues.json())
-    print(f'Loaded {count} issues. Loading ZenHub data...')
-
-    issues_list = []
-    for issue in issues.json():
-        # Ignore pull requests, which github treats as issues
-        if 'pull_request' in issue:
-            continue
-
-        zen_r = query_zenhub_issue(repo_name, issue['number'], config).json()
-
-        print(f'{repo_name} issue: ' + str(issue['number']))
-        issues_list.append(process_issue(issue, zen_r))
-
-        # DEBUG
-        #rprint(issue)
-        #rprint(zen_r)
-
-
-    write_issues(issues_list, config['FILEOUTPUT'])
-
-    rprint(issues.headers['link'])
-
-    if 'link' in issues.headers:
-        link_header = issues.headers['link']
-        pages = get_pages(link_header)
-        rprint(pages)
-        exit()
-
-
-        # @TODO: wrap this in an iterator?
-        # this goes away in the iterator..
-        while 'last' in pages and 'next' in pages:
-            pages = get_pages()
-            pages = dict(
-                [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
-                 [link.split(';') for link in
-                  issues.headers['link'].split(',')]])
-
-            issues = get_github_api_raw(pages['next'], config)
-            write_issues(issues, config['FILEOUTPUT'], repo_name, repo_ID, config['ZENHUB_AUTH'])
-
-            if pages['next'] == pages['last']:
-                break
-            
-def get_pages(link_header):
-    return dict(
-        [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
-            [link.split(';') for link in
-            link_header.split(',')]])
-
+    issue_iter = HubIssues(repo_name, config)
+    for issues in issue_iter:
+        write_issues(issues, config['FILEOUTPUT'])
 
 def write_issues(issues, csvout):
     for issue in issues:
         csvout.writerow(prepare_row(issue))
-
-def process_issue(issue, zen_r):
-    #DateCreated = issue['created_at'][:-10]
-    #DateUpdated = issue['updated_at'][:-10]
-
-    DateCreated = parse_date(issue['created_at'])
-    DateUpdated = parse_date(issue['updated_at'])
-
-    assignees, tags, category, priority, labels = '', '', '', '', ''
-
-    for i in issue['assignees'] if issue['assignees'] else []:
-        assignees += i['login'] + ','
-
-    for x in issue['labels'] if issue['labels'] else []:
-        tags += x['name'] + ','
-
-    estimate = zen_r.get('estimate', dict()).get('value', "")
-
-    if issue['state'] == 'closed':
-        Pipeline = 'Closed'
-    else:
-        Pipeline = zen_r.get('pipeline', dict()).get('name', "")
-
-    if not issue.get('body'):
-        issue['body'] = ''
-
-    issue['is_epic'] = zen_r['is_epic'] if "is_epic" in zen_r else False
-
-    row_dict = hub_row_data(issue, tags, assignees, priority, DateCreated, DateUpdated, estimate)
-    return row_dict
-
 
 if __name__ == '__main__':
     main()
